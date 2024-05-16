cea8818ecc4a6d2eb73853cd66b4d193e2bef1c2
JinUng41 <ingjwjw@naver.com>
2024-05-16 21:18:07 +0900

    [Config] 프로젝트 파일 추가

---
+++ Caculator_SwiftUI/Caculator_SwiftUI/Caculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Caculator_SwiftUIApp.swift
+//  Caculator_SwiftUI
+//
+//  Created by 김진웅 on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Caculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Caculator_SwiftUI/Caculator_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Caculator_SwiftUI
+//
+//  Created by 김진웅 on 5/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
