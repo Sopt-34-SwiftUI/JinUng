e0d4b258b2b88df8194d3ce6f08e14eef7d5462c
JinUng41 <ingjwjw@naver.com>
2024-05-21 22:28:19 +0900

    [Refactor] 버튼 스타일 배열 클래스 내 저장 프로퍼티로 선언

--- Caculator_SwiftUI/Caculator_SwiftUI/ContentView.swift
+++ Caculator_SwiftUI/Caculator_SwiftUI/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let buttons: [[CalculateButtonStyle]] = [
+        [.ac, .plusMinus, .percent, .divider],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .subtract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal]
+    ]
+    
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
@@ -22,14 +30,6 @@ struct ContentView: View {
                         .font(.system(size: 75))
                         .foregroundStyle(.white)
                 }
-                
-                let buttons: [[CalculateButtonStyle]] = [
-                    [.ac, .plusMinus, .percent, .divider],
-                    [.seven, .eight, .nine, .multiply],
-                    [.four, .five, .six, .subtract],
-                    [.one, .two, .three, .add],
-                    [.zero, .decimal, .equal]
-                ]
                 
                 ForEach(buttons, id: \.self) { row in
                     CalculateButtonRow(buttonRows: row)
