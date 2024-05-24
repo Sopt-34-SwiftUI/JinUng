ffe1f8d01b11096206564e5e5359810290ec28b4
JinUng41 <ingjwjw@naver.com>
2024-05-24 19:58:44 +0900

    [Feat] 계산 로직을 수행할 메서드와 State 프로퍼티 선언 및 적용

--- Caculator_SwiftUI/Caculator_SwiftUI/ContentView.swift
+++ Caculator_SwiftUI/Caculator_SwiftUI/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentInput: Double = 0
+    @State private var previousInput: Double = 0
+    @State private var currentOperation: CalculateButtonStyle?
+    @State private var inputValue = "0"
+    
     private let buttons: [[CalculateButtonStyle]] = [
         [.ac, .plusMinus, .percent, .divider],
         [.seven, .eight, .nine, .multiply],
@@ -25,16 +30,70 @@ struct ContentView: View {
                 
                 HStack {
                     Spacer()
-                    Text("331,460")
+                    Text(inputValue)
                         .padding()
                         .font(.system(size: 75))
                         .foregroundStyle(.white)
                 }
                 
                 ForEach(buttons, id: \.self) { row in
-                    CalculateButtonRow(buttonRows: row)
+                    CalculateButtonRow(buttonRows: row, action: buttonPressed)
                 }
             }
+        }
+    }
+    
+    func buttonPressed(_ style: CalculateButtonStyle) {
+        guard let input = Double(inputValue) else { return }
+        switch style {
+        case .ac:
+            currentInput = 0
+            previousInput = 0
+            currentOperation = nil
+            inputValue = "0"
+        case .plusMinus:
+            inputValue = "-\(input)"
+        case .percent:
+            inputValue = "\(input / 100)"
+        case .divider, .multiply, .subtract, .add:
+            previousInput = input
+            currentOperation = style
+            inputValue = "0"
+        case .equal:
+            guard let operation = currentOperation else { return }
+            let result = performOperation(
+                previous: previousInput,
+                current: input,
+                operation: operation
+            )
+            inputValue = "\(result)"
+            previousInput = result
+            currentOperation = nil
+        default:
+            if inputValue == "0" {
+                inputValue = style.stringValue
+            } else {
+                inputValue += style.stringValue
+            }
+        }
+    }
+    
+    func performOperation(
+        previous: Double,
+        current: Double,
+        operation: CalculateButtonStyle
+    ) -> Double {
+        switch operation {
+        case .add:
+            return previous + current
+        case .subtract:
+            return previous - current
+        case .multiply:
+            return previous * current
+        case .divider:
+            return previous / current
+        default:
+            fatalError("Unsupported operation")
         }
     }
 }
