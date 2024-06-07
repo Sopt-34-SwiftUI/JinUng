a25ebf41be249ddded241d890c47ab317dba7194
JinUng41 <ingjwjw@naver.com>
2024-06-07 10:17:42 +0900

    [Refactor] 계산기 화면을 한 단계 더 분리

--- Caculator_SwiftUI/Caculator_SwiftUI/ContentView.swift
+++ Caculator_SwiftUI/Caculator_SwiftUI/ContentView.swift
@@ -8,39 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var viewModel: CalculateViewModel = CalculateViewModel()
-    
-    private let buttons: [[CalculateButtonStyle]] = [
-        [.ac, .plusMinus, .percent, .divider],
-        [.seven, .eight, .nine, .multiply],
-        [.four, .five, .six, .subtract],
-        [.one, .two, .three, .add],
-        [.zero, .decimal, .equal]
-    ]
-    
     var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
-            
-            VStack {
-                Spacer()
-                
-                HStack {
-                    Spacer()
-                    Text(viewModel.displayText)
-                        .padding()
-                        .font(.system(size: 75))
-                        .foregroundStyle(.white)
-                }
-                
-                ForEach(buttons, id: \.self) { row in
-                    CalculateButtonRow(
-                        buttonRows: row,
-                        action: viewModel.buttonPressed(_:)
-                    )
-                }
-            }
-        }
+        CalculateView()
     }
 }
 
---
+++ Caculator_SwiftUI/Caculator_SwiftUI/View/CalculateView.swift
@@ -0,0 +1,45 @@
+//
+//  CalculateView.swift
+//  Caculator_SwiftUI
+//
+//  Created by 김진웅 on 6/7/24.
+//
+
+import SwiftUI
+
+struct CalculateView: View {
+    @StateObject private var viewModel: CalculateViewModel = CalculateViewModel()
+    
+    private let buttons: [[CalculateButtonStyle]] = [
+        [.ac, .plusMinus, .percent, .divider],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .subtract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal]
+    ]
+    
+    var body: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Text(viewModel.displayText)
+                        .padding()
+                        .font(.system(size: 75))
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(buttons, id: \.self) { row in
+                    CalculateButtonRow(
+                        buttonRows: row,
+                        action: viewModel.buttonPressed(_:)
+                    )
+                }
+            }
+        }
+    }
+}
